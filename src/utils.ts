d5285299e2233944a26000be3054bab8aca8f231
LuisNivar <luisjosenivarn@hotmail.com>
2024-12-22 22:38:01 -0400

    Add Run Dev flag

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,8 @@ export function FormatDate(date: Date): string {
   return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
 }
 
+export const IS_DEV = import.meta.env.MODE === "development";
+
 /**
  * To add hour
  * ${date.getHours()}:${date

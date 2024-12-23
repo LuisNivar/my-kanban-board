db7ab72f84c78d22ca0a49d89ebe1888ecf85f2f
LuisNivar <luisjosenivarn@hotmail.com>
2024-12-23 19:15:27 -0400

    -Add SubMenu -Add Checkbox items

--- src/components/UI/DropdownMenu.tsx
+++ src/components/UI/DropdownMenu.tsx
@@ -1,12 +1,13 @@
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 import { PropsWithChildren } from "react";
+import { GoCheck as CheckIcon } from "react-icons/go";
 
 function DropdownMenu({ children }: PropsWithChildren) {
   return <DropdownMenuPrimitive.Root>{children}</DropdownMenuPrimitive.Root>;
 }
 
 export type OptionProps = DropdownMenuPrimitive.DropdownMenuItemProps & {
-  variant: "default" | "danger" | "disable";
+  variant: "default" | "danger";
 };
 
 function Separator() {
@@ -15,7 +16,7 @@ function Separator() {
   );
 }
 
-function Options({ children, variant, ...props }: OptionProps) {
+function Item({ children, variant, ...props }: OptionProps) {
   const styleDefault =
     "gap-2 text-neutral-300 group flex select-none items-center rounded-md px-3 py-2 text-sm leading-none outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-neutral-700/60 data-[disabled]:text-neutral-500 data-[highlighted]:text-neutral-100";
 
@@ -26,10 +27,28 @@ function Options({ children, variant, ...props }: OptionProps) {
     <DropdownMenuPrimitive.Item
       className={variant === "danger" ? styleDanger : styleDefault}
       {...props}
-      disabled={variant === "disable"}
     >
       {children}
     </DropdownMenuPrimitive.Item>
+  );
+}
+
+export type CheckboxItemProps =
+  DropdownMenuPrimitive.DropdownMenuCheckboxItemProps & {
+    label: string;
+  };
+
+function CheckboxItem({ label, ...props }: CheckboxItemProps) {
+  return (
+    <DropdownMenuPrimitive.CheckboxItem
+      className="gap-2 text-neutral-300 group flex select-none items-center rounded-md px-6 py-2 text-sm leading-none outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-neutral-700/60 data-[disabled]:text-neutral-500 data-[highlighted]:text-neutral-100"
+      {...props}
+    >
+      <DropdownMenuPrimitive.ItemIndicator className="absolute left-3">
+        <CheckIcon />
+      </DropdownMenuPrimitive.ItemIndicator>
+      {label}
+    </DropdownMenuPrimitive.CheckboxItem>
   );
 }
 
@@ -52,9 +71,39 @@ function Content({ children }: PropsWithChildren) {
   );
 }
 
+function SubMenu({ children }: PropsWithChildren) {
+  return <DropdownMenuPrimitive.Sub>{children}</DropdownMenuPrimitive.Sub>;
+}
+
+function SubContent({ children }: PropsWithChildren) {
+  return (
+    <DropdownMenuPrimitive.Portal>
+      <DropdownMenuPrimitive.SubContent
+        className="min-w-fit rounded-lg flex flex-col gap-0.5 border-neutral-600 bg-neutral-800 border px-2 py-2 shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade data-[side=right]:animate-slideLeftAndFade data-[side=top]:animate-slideDownAndFade"
+        sideOffset={2}
+        alignOffset={-5}
+      >
+        {children}
+      </DropdownMenuPrimitive.SubContent>
+    </DropdownMenuPrimitive.Portal>
+  );
+}
+
+function SubTrigger({ children }: PropsWithChildren) {
+  return (
+    <DropdownMenuPrimitive.SubTrigger className="group relative flex px-3 py-2 select-none items-center rounded-md pl-8 pr-1 text-sm leading-none text-neutral-300 outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-neutral-700/60 data-[highlighted]:data-[state=open]:bg-neutral-700/60 data-[state=open]:bg-teal-400/10 data-[disabled]:text-neutral-500 data-[highlighted]:data-[state=open]:text-neutral-100 data-[highlighted]:text-neutral-100 data-[state=open]:text-teal-200">
+      {children}
+    </DropdownMenuPrimitive.SubTrigger>
+  );
+}
+
 export default Object.assign(DropdownMenu, {
-  Options,
+  Item,
+  CheckboxItem,
   Trigger,
   Content,
   Separator,
+  SubMenu,
+  SubContent,
+  SubTrigger,
 });

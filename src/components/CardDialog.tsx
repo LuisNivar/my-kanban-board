00ef71bb03299ff33e74af9b0b17d42c1094baec
LuisNivar <luisjosenivarn@hotmail.com>
2024-12-31 13:04:27 -0400

    Add Tag Selector to CardDialog

--- src/components/CardDialog.tsx
+++ src/components/CardDialog.tsx
@@ -1,12 +1,14 @@
-import { PropsWithChildren, useContext, useState } from "react";
+import { PropsWithChildren, useContext, useEffect, useState } from "react";
 import { ColumProps, ItemProps, Tag } from "../types";
 import Dialog from "./UI/Dialog";
 import { CardDispatchContext } from "../Context";
 import { FormatDate } from "../utils";
+import { TagSelector } from "./TagSelector";
 
 export type CardDialogProps = PropsWithChildren & {
   column: ColumProps["name"];
 };
+
 export function CardDialog({ children, column }: CardDialogProps) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -39,20 +41,20 @@ export function CardDialog({ children, column }: CardDialogProps) {
       newCard: newCard,
     });
 
-    //reset
+    Initialize();
+  }
+
+  function Initialize() {
     setTags(INITIAL_TAGS_STATE);
     setDescription("");
     setTitle("");
   }
   return (
     <Dialog>
-      <Dialog.Trigger>{children}</Dialog.Trigger>
+      <Dialog.Trigger onClick={() => Initialize()}>{children}</Dialog.Trigger>
       <Dialog.Content title="New task">
         <div className="flex flex-col gap-3">
-          <fieldset className="flex items-center gap-5">
-            <label className="w-24 text-right text-sm" htmlFor="title">
-              Title
-            </label>
+          <Field label="Title" htmlFor="title">
             <input
               className="inline-flex w-full py-2 flex-1 border border-neutral-600 bg-neutral-900/60 items-center justify-center rounded px-3 text-sm leading-none outline-none selection:bg-teal-600"
               id="title"
@@ -60,19 +62,18 @@ export function CardDialog({ children, column }: CardDialogProps) {
               placeholder="New task..."
               autoFocus
             />
-          </fieldset>
-          <fieldset className="flex items-center gap-5">
-            <label className="w-24 text-right text-sm" htmlFor="description">
-              Description
-            </label>
+          </Field>
+          <Field label="Description" htmlFor="description">
             <textarea
               className=" max-h-24 inline-flex w-full py-2 flex-1 border border-neutral-600 bg-neutral-900/60 items-center justify-center rounded px-3 text-sm leading-none outline-none selection:bg-teal-600"
               id="description"
               onChange={(e) => setDescription(e.target.value)}
               placeholder="Describe your task. **Optional**"
             />
-          </fieldset>
-
+          </Field>
+          <Field label="Tags" htmlFor="tags">
+            <TagSelector state={tags} setTags={setTags} />
+          </Field>
           <Dialog.Button
             className="self-end w-fit"
             onClick={() => handleSave()}
@@ -82,5 +83,17 @@ export function CardDialog({ children, column }: CardDialogProps) {
         </div>
       </Dialog.Content>
     </Dialog>
+  );
+}
+
+type FieldProps = PropsWithChildren & { label: string; htmlFor: string };
+function Field({ children, label, htmlFor }: FieldProps) {
+  return (
+    <fieldset className="flex items-center gap-5">
+      <label className="w-24 text-right text-sm" htmlFor={htmlFor}>
+        {label}
+      </label>
+      {children}
+    </fieldset>
   );
 }
--- src/components/TagSelector.tsx
+++ src/components/TagSelector.tsx
@@ -1,53 +1,56 @@
+import { useEffect } from "react";
 import { TagSelectorProps } from "../types";
 
 export function TagSelector({ state, setTags }: TagSelectorProps) {
-  return (
-    <div className="flex items-center gap-1.5 mb-4">
-      <span className="ml-2 text-xs text-neutral-400">Select Tags: </span>
+  useEffect(() => {
+    setTags(state);
+  }, []);
 
-      <div
+  return (
+    <span className="flex items-center gap-1.5">
+      <button
         onClick={() => {
           setTags({ ...state, red: !state.red });
         }}
-        className={`w-8 h-3 cursor-pointer rounded border border-rose-500  ${
+        className={`w-10 h-4 cursor-pointer rounded border border-rose-500  ${
           state.red
             ? "bg-rose-500 hover:bg-rose-400"
             : "bg-neutral-800 hover:bg-rose-500/40"
         }`}
-      ></div>
+      ></button>
 
-      <div
+      <button
         onClick={() => {
           setTags({ ...state, yellow: !state.yellow });
         }}
-        className={`w-8 h-3 cursor-pointer rounded border border-yellow-500  ${
+        className={`w-10 h-4 cursor-pointer rounded border border-yellow-500  ${
           state.yellow
             ? "bg-yellow-500 hover:bg-yellow-400"
             : "bg-neutral-800 hover:bg-yellow-500/40"
         }`}
-      ></div>
+      ></button>
 
-      <div
+      <button
         onClick={() => {
           setTags({ ...state, green: !state.green });
         }}
-        className={`w-8 h-3 cursor-pointer rounded border border-green-500  ${
+        className={`w-10 h-4 cursor-pointer rounded border border-green-500  ${
           state.green
             ? "bg-green-500 hover:bg-green-400"
             : "bg-neutral-800 hover:bg-green-500/40"
         }`}
-      ></div>
+      ></button>
 
-      <div
+      <button
         onClick={() => {
           setTags({ ...state, blue: !state.blue });
         }}
-        className={`w-8 h-3 cursor-pointer rounded border border-blue-500  ${
+        className={`w-10 h-4 cursor-pointer rounded border border-blue-500  ${
           state.blue
             ? "bg-blue-500 hover:bg-blue-400"
             : "bg-neutral-800 hover:bg-blue-500/40"
         }`}
-      ></div>
-    </div>
+      ></button>
+    </span>
   );
 }

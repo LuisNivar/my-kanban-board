651a46e320b8c9796919959e1bc2c3ffb7a6b51a
LuisNivar <luisjosenivarn@hotmail.com>
2025-01-05 17:23:34 -0400

    Add 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,7 @@ import { Kanban } from "./components/Kanban.tsx";
 import "./index.css";
 import Settings from "./pages/Settings.tsx";
 import Home from "./pages/home.tsx";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//     children: [
-//       { path: "/", element: <Kanban /> },
-//       { path: "/settings", element: <Settings /> },
-//     ],
-//   },
-// ]);
+import NotFoundPage from "./pages/NoFoundPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -26,6 +16,7 @@ createRoot(document.getElementById("root")!).render(
           <Route path="/" element={<Home />} />
           <Route path="/board/:id" element={<Kanban />} />
           <Route path="/settings" element={<Settings />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>

e94e6d36a0a378440e4924d9265ff425e15a4387
LuisNivar <luisjosenivarn@hotmail.com>
2025-01-11 15:28:31 -0400

    Agrupate Icons in one file

---
+++ src/components/Icons.tsx
@@ -0,0 +1,50 @@
+import {
+  GoRepo as BookIcon,
+  GoCalendar as CalendarIcon,
+  GoCodeSquare as CodeIcon,
+  GoDatabase as DataBaseIcon,
+  GoFileDirectory as FolderIcon,
+  GoHeart as HeartIcon,
+  GoStar as StarIcon,
+} from "react-icons/go";
+import { PiHammer as HammerIcon } from "react-icons/pi";
+import { VscGame as GameIcon } from "react-icons/vsc";
+
+import {
+  GoPackage as BoxIcon,
+  GoPin as PinIcon,
+  GoTrophy as TrophyIcon,
+} from "react-icons/go";
+
+import {
+  GoPlus as AddIcon,
+  GoAlert as AlertIcon,
+  GoChevronRight as ChevronIcon,
+  GoKebabHorizontal as MoreIcon,
+  GoTrash as TrashIcon,
+} from "react-icons/go";
+import { GrEdit as EditIcon } from "react-icons/gr";
+
+import { HiOutlineFire as FireIcon } from "react-icons/hi2";
+
+export {
+  AddIcon,
+  AlertIcon,
+  BookIcon,
+  BoxIcon,
+  CalendarIcon,
+  ChevronIcon,
+  CodeIcon,
+  DataBaseIcon,
+  EditIcon,
+  FireIcon,
+  FolderIcon,
+  GameIcon,
+  HammerIcon,
+  HeartIcon,
+  MoreIcon,
+  PinIcon,
+  StarIcon,
+  TrashIcon,
+  TrophyIcon,
+};

86f72dc6f03cc9c78fb1ea4726e6847d54257413
LuisNivar <luisjosenivarn@hotmail.com>
2025-01-11 12:59:35 -0400

    Improve layout

--- src/components/Sidebar/Header.tsx
+++ src/components/Sidebar/Header.tsx
@@ -3,7 +3,7 @@ import Separator from "./Separator";
 
 export default function Header() {
   return (
-    <>
+    <div className="flex gap-3 items-center flex-col">
       <Link
         to={"/"}
         className="cursor-pointer select-none bg-yellow-500 hover:bg-yellow-300 transition-colors text-xs font-medium text-neutral-800 px-2 py-1 rounded-md"
@@ -11,6 +11,6 @@ export default function Header() {
         WIP
       </Link>
       <Separator />
-    </>
+    </div>
   );
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,20 +1,26 @@
 import { SidebarProvider } from "../../SidebarProvider";
 import AddButtonItem from "./AddButtonItem";
-import Body from "./Body";
 import Footer from "./Footer";
 import Header from "./Header";
+import Items from "./Items";
 
 export default function Sidebar() {
   return (
     <SidebarProvider>
       <div className="flex justify-between text-[22px] text-neutral-400 flex-col items-center gap-3 py-3 px-2 rounded-lg h-screen bg-neutral-800">
-        <nav className="flex flex-col items-center gap-3">
-          <Header />
-          <Body />
-          <AddButtonItem />
-        </nav>
+        <Header />
+        <Body />
         <Footer />
       </div>
     </SidebarProvider>
+  );
+}
+
+function Body() {
+  return (
+    <nav className="flex grow flex-col items-center gap-3">
+      <Items />
+      <AddButtonItem />
+    </nav>
   );
 }

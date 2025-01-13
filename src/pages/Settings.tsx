24b38353bc786b577c4b744444b08609307989d4
LuisNivar <luisjosenivarn@hotmail.com>
2025-01-13 14:33:30 -0400

    Add Sections container

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -4,7 +4,9 @@ export default function Settings() {
   return (
     <div className="h-screen w-full py-12 px-32 bg-neutral-900 text-neutral-400">
       <h1 className="font-bold text-3xl mb-6 text-neutral-300">Settings</h1>
-      <SidebarSection />
+      <div className="flex items-start gap-2">
+        <SidebarSection />
+      </div>
     </div>
   );
 }

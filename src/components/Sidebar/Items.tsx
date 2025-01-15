5c964f858055ab8828adac8bc4527c0738fe5728
LuisNivar <luisjosenivarn@hotmail.com>
2025-01-14 22:41:40 -0400

    Add Tooltip and context menu to sidebar items

--- src/components/Sidebar/Items.tsx
+++ src/components/Sidebar/Items.tsx
@@ -1,19 +1,30 @@
 import { nanoid } from "nanoid";
 import { SideBarItemLink } from "../../types";
-import LinkItem from "./LinkItem";
+import { LinkItem } from "./LinkItem";
+import { SidebarMenuAction } from "./SidebarMenuAction";
+import Tooltip from "../UI/Tooltip";
+import { ForwardPropsToChild } from "../UI/ForwardPropsToChild";
 
 type itemsProps = { state: SideBarItemLink[] };
 export default function Items({ state }: itemsProps) {
   return (
     <>
       {state.map((item) => (
-        <LinkItem
-          key={nanoid()}
-          to={item.path}
-          icon={item.icon}
-          name={item.name}
-        />
+        <Item key={nanoid()} {...item} />
       ))}
     </>
+  );
+}
+
+function Item(props: SideBarItemLink) {
+  const { name, icon, path } = props;
+  return (
+    <SidebarMenuAction>
+      <ForwardPropsToChild>
+        <Tooltip text={name}>
+          <LinkItem to={path} icon={icon} name={name} />
+        </Tooltip>
+      </ForwardPropsToChild>
+    </SidebarMenuAction>
   );
 }

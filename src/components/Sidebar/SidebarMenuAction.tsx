24fdb6c982c0ef6aea21ce6de53b4d98c53318e4
LuisNivar <luisjosenivarn@hotmail.com>
2025-01-14 23:08:26 -0400

    Add Actions componet to SidebarMenuAction

--- src/components/Sidebar/SidebarMenuAction.tsx
+++ src/components/Sidebar/SidebarMenuAction.tsx
@@ -1,13 +1,47 @@
-import ContextMenu, { ContextMenuProps } from "../UI/ContextMenu";
+import React from "react";
+import { SettingIcon, TrashIcon } from "../Icons";
+import ContextMenu, { ContextMenuProps, ItemProps } from "../UI/ContextMenu";
 
-export function SidebarMenuAction({ children, ...props }: ContextMenuProps) {
+type SiSidebarMenuActionProps = ContextMenuProps & {
+  onDelete: () => void;
+  onEdit: () => void;
+};
+export function SidebarMenuAction({
+  children,
+  onDelete,
+  onEdit,
+  ...props
+}: SiSidebarMenuActionProps) {
   return (
     <ContextMenu {...props}>
       <ContextMenu.Trigger asChild>{children}</ContextMenu.Trigger>
       <ContextMenu.Content>
-        <ContextMenu.Item variant="default">Test</ContextMenu.Item>
-        <ContextMenu.Item variant="danger">Remove</ContextMenu.Item>
+        <Actions
+          variant="default"
+          label="Sidebar setup..."
+          icon={<SettingIcon />}
+          onClick={() => onEdit()}
+        />
+        <Actions
+          variant="danger"
+          label="Delete"
+          icon={<TrashIcon />}
+          onClick={() => onDelete()}
+        />
       </ContextMenu.Content>
     </ContextMenu>
+  );
+}
+
+type ActionsProps = ItemProps & { label: string; icon?: React.ReactNode };
+function Actions({ label, icon, variant, ...props }: ActionsProps) {
+  return (
+    <ContextMenu.Item variant={variant} {...props}>
+      <button
+        className={`flex items-center gap-2 w-full ${!icon ? "ml-6" : ""}`}
+      >
+        {icon} <span>{label}</span>
+      </button>
+    </ContextMenu.Item>
   );
 }

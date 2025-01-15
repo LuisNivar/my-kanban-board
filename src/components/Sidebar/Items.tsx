98a42d3ee7c3a14b91cd85fa5e933b5e431ba554
LuisNivar <luisjosenivarn@hotmail.com>
2025-01-14 23:10:05 -0400

    Add Event handlers to manage sidebar-menu actions

--- src/components/Sidebar/Items.tsx
+++ src/components/Sidebar/Items.tsx
@@ -4,6 +4,9 @@ import { LinkItem } from "./LinkItem";
 import { SidebarMenuAction } from "./SidebarMenuAction";
 import Tooltip from "../UI/Tooltip";
 import { ForwardPropsToChild } from "../UI/ForwardPropsToChild";
+import { useContext } from "react";
+import { SidebarDispatchContext } from "../../Context";
+import { useNavigate } from "react-router-dom";
 
 type itemsProps = { state: SideBarItemLink[] };
 export default function Items({ state }: itemsProps) {
@@ -18,8 +21,20 @@ export default function Items({ state }: itemsProps) {
 
 function Item(props: SideBarItemLink) {
   const { name, icon, path } = props;
+  const dispatch = useContext(SidebarDispatchContext);
+  const navigate = useNavigate();
+
+  //TODO: Add Delet confimation
+  function handleDelete() {
+    dispatch({ type: "delete", icon });
+  }
+
+  function handleEdit() {
+    navigate("/settings");
+  }
+
   return (
-    <SidebarMenuAction>
+    <SidebarMenuAction onDelete={handleDelete} onEdit={handleEdit}>
       <ForwardPropsToChild>
         <Tooltip text={name}>
           <LinkItem to={path} icon={icon} name={name} />

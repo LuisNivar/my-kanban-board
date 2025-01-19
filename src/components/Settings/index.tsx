8489eaf309d5e6f11841860d4262124dd94437f2
LuisNivar <luisjosenivarn@hotmail.com>
2025-01-19 11:32:52 -0400

    group in one component the sections settings

---
+++ src/components/Settings/index.tsx
@@ -0,0 +1,16 @@
+import BackgrounbdSection from "./BackgroundSection";
+import SidebarSection from "./SidebarSection";
+
+export default function SettingsContent() {
+  return (
+    <div className="h-screen w-full py-12 px-32 bg-neutral-900 text-neutral-400">
+      <h1 className="font-bold text-3xl mb-6 text-neutral-100 [text-shadow:_0_2px_4px_rgb(22_22_22_/_0.8)]">
+        Settings
+      </h1>
+      <div className="flex items-start gap-3">
+        <SidebarSection />
+        <BackgrounbdSection />
+      </div>
+    </div>
+  );
+}

ce4781a7f315cdb30aaf53ed7bd6756effd7e1b5
LuisNivar <luisjosenivarn@hotmail.com>
2025-01-21 08:40:01 -0400

    Use clsx in column header component

--- src/components/Column/ColumnHeader.tsx
+++ src/components/Column/ColumnHeader.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 type ColumnHeaderProps = {
   count: number;
   title: string;
@@ -11,7 +13,7 @@ export default function ColumnHeader({
 }: ColumnHeaderProps) {
   return (
     <header className="select-none px-3 py-2 rounded-lg cursor-default shadow-[0_2px_2px] shadow-neutral-900/80  bg-neutral-800 gap-4 flex items-center justify-center">
-      <h3 className={`font-medium ${color}`}>{title}</h3>
+      <h3 className={clsx("font-medium", color)}>{title}</h3>
       <span className="text-center text-sm text-neutral-400">{count ?? 0}</span>
     </header>
   );

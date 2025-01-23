ddc80d8285286df29d7de4057444c015a9556d0d
LuisNivar <luisjosenivarn@hotmail.com>
2025-01-22 21:47:48 -0400

    Add dictionary to the headings colors

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,14 @@ export function FormatDate(date: Date): string {
 }
 
 export const IS_DEV = import.meta.env.MODE === "development";
+
+export const COLOR_HEADING_DICTIONARY: {
+  [color: string]: { text: string; bg: string };
+} = {
+  ["neutral"]: { text: "text-neutral-400", bg: "bg-neutral-400" },
+  ["red"]: { text: "text-red-400", bg: "bg-red-400" },
+  ["purple"]: { text: "text-purple-400", bg: "bg-purple-400" },
+  ["cyan"]: { text: "text-cyan-500", bg: "bg-cyan-500" },
+  ["emerald"]: { text: "text-emerald-500", bg: "bg-emerald-500" },
+  ["amber"]: { text: "text-amber-500", bg: "bg-amber-500" },
+};

3ccd8d8753dc9524c7499c0cd5f91913d2f885e4
LuisNivar <luisjosenivarn@hotmail.com>
2025-01-24 17:33:32 -0400

    Add home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,20 @@
+import HomeContent from "../components/HomePage";
+
 export default function Home() {
   return (
-    <section className="h-screen py-12 px-36 bg-neutral-900 text-neutral-400">
-      <h1 className="font-bold text-3xl mb-4 text-neutral-300">Home</h1>
-      <article className="flex gap-3 flex-col">
-        <p>
-          Dolore Lorem nulla non eu elit cillum laboris incididunt incididunt
-          consectetur. Proident minim pariatur ipsum culpa consectetur in
-          officia. Eu elit anim sint aliquip ipsum magna dolore ut. Consequat
-          nostrud aliquip cupidatat anim ipsum aute aliquip aliqua aliqua
-          reprehenderit.
-        </p>
-      </article>
+    <section className="select-none flex flex-col gap-3 h-screen w-full py-12 px-36 bg-neutral-900 text-neutral-400">
+      <h1 className=" font-bold text-5xl mb-10 text-neutral-300">
+        MY{" "}
+        <span className="bg-gradient-to-r from-teal-500 from- via-blue-500 via- bg-clip-text text-transparent inline-block">
+          KANBAN
+        </span>
+        BOARD
+      </h1>
+      <HomeContent />
+
+      <p className=" text-lg italic text-neutral-500">
+        Just select one and begin to plan!
+      </p>
     </section>
   );
 }

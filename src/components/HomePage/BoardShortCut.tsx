10de5f097b9eb475765a4d19c685c8529c318e97
LuisNivar <luisjosenivarn@hotmail.com>
2025-01-24 20:40:17 -0400

    Improve home page boards style

--- src/components/HomePage/BoardShortCut.tsx
+++ src/components/HomePage/BoardShortCut.tsx
@@ -1,6 +1,6 @@
+import clsx from "clsx";
 import { Link } from "react-router-dom";
 import { DEFAULT_BACKGROUND } from "../Settings/utils";
-import clsx from "clsx";
 import { ICON_DICTIONARY } from "../Sidebar/utils";
 
 type BoardsProps = {
@@ -19,13 +19,13 @@ export default function BoardShortCut({
   return (
     <Link
       className={clsx(
-        "group flex-shrink-0 overflow-hidden bg-cover flex flex-col w-56 h-40 justify-end rounded-lg text-neutral-400",
+        "group flex-shrink-0 border transition-colors border-neutral-750 hover:border-teal-900 overflow-hidden bg-cover flex flex-col w-56 h-40 justify-end rounded-lg text-neutral-400",
         background ?? DEFAULT_BACKGROUND
       )}
       rel="preload"
       to={path}
     >
-      <span className="flex items-center text-base gap-2 transition-colors bg-neutral-900/80 self-start px-3 py-1 w-full group-hover:bg-teal-500/20 group-hover:text-teal-300">
+      <span className="group-hover:backdrop-blur-[2px] flex items-center text-base gap-2  bg-neutral-900/80 self-start px-3 py-1 w-full group-hover:bg-teal-900/75 group-hover:text-teal-200">
         {ICON_DICTIONARY[icon]}
         {name}
       </span>
